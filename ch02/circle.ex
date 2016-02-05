6d0374a6b45e8d2da0e50611baa9ec7376753ea2
Thomas Chen <chen.thomas.c@gmail.com>
2016-02-05 15:06:19 -0500

    define some module attributes

---
+++ ch02/circle.ex
@@ -0,0 +1,11 @@
+defmodule Circle do
+  @moduledoc "Implements basis circle functions"
+
+  @pi 3.14159
+
+  @doc "Computes the area of a circle"
+  def area(r), do: r*r*@pi
+
+  @doc "Computes the circumference of a circle"
+  def circumference(r), do: 2*r*@pi
+end

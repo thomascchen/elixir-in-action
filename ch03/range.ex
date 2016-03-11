beeb64cefaf4f2b53b19a76be0a5a5fb0aefb062
Thomas Chen <chen.thomas.c@gmail.com>
2016-03-11 12:56:20 -0500

    Add range function using tail recursion

---
+++ ch03/range.ex
@@ -0,0 +1,14 @@
+defmodule Range do
+  def range(from, to) do
+    count(from, to)
+  end
+
+  defp count(from, to) when from === to do
+    IO.puts(from)
+  end
+
+  defp count(from, to) do
+    IO.puts(from)
+    count(from + 1, to)
+  end
+end

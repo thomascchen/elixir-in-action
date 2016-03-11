cf01e858d26801bcb0dcd2639ae9e9016f47ff3e
Thomas Chen <chen.thomas.c@gmail.com>
2016-03-11 12:44:11 -0500

    Add list length tail recursive function

---
+++ ch03/list_len.ex
@@ -0,0 +1,18 @@
+defmodule ListLength do
+  def count(list) when is_list(list) do
+    counter(0, list)
+  end
+
+  def count(n) do
+    IO.puts("not a list!")
+  end
+
+  defp counter(accumulator, []) do
+    accumulator
+  end
+
+  defp counter(accumulator, [_ | tail]) do
+    new_length = accumulator + 1
+    counter(new_length, tail)
+  end
+end

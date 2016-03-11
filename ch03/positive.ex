80016171d312eac4c6d181b544a4b7ee8c996597
Thomas Chen <chen.thomas.c@gmail.com>
2016-03-11 15:02:04 -0500

    Add function that takes a list and returns another list that contains only positive numbers from the input list

---
+++ ch03/positive.ex
@@ -0,0 +1,18 @@
+defmodule PositiveNum do
+  def filter(list) do
+    eval_list([], list)
+  end
+
+  defp eval_list(new_list, []) do
+    new_list
+  end
+
+  defp eval_list(new_list, [head | tail]) when is_number(head) and head > 0 do
+    positive_list = new_list ++ [head]
+    eval_list(positive_list, tail)
+  end
+
+  defp eval_list(new_list, [head | tail]) do
+    eval_list(new_list, tail)
+  end
+end

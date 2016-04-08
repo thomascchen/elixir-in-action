62697d3be5d5da41224368331f30615d78c0954b
Thomas Chen <chen.thomas.c@gmail.com>
2016-04-08 19:44:55 -0400

    Represent entries with maps

--- ch04/todo-multi_dict.ex
+++ ch04/todo-multi_dict.ex
@@ -14,3 +14,15 @@ defmodule MultiDict do
     Map.get(dict, key, [])
   end
 end
+
+defmodule TodoList do
+  def new, do: MultiDict.new
+
+  def add_entry(todo_list, entry) do
+    MultiDict.add(todo_list, entry.date, entry)
+  end
+
+  def entries(todo_list, date) do
+    MultiDict.get(todo_list, date)
+  end
+end

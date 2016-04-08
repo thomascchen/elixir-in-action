0b730881a24a0ebc88491599ec2e186232e7fd6e
Thomas Chen <chen.thomas.c@gmail.com>
2016-04-08 19:44:46 -0400

    Delete trailing white space

--- ch04/simple_todo.ex
+++ ch04/simple_todo.ex
@@ -2,7 +2,7 @@ defmodule TodoList do
   def new, do: MultiDict.new
 
   def add_entry(todo_list, date, title) do
-    MultiDict.add(todo_list, date, title) 
+    MultiDict.add(todo_list, date, title)
   end
 
   def entries(todo_list, date) do

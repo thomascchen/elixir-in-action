29bd7ad10231de6e78320e69756052a17fbd7bc3
Thomas Chen <chen.thomas.c@gmail.com>
2016-04-08 16:38:21 -0400

    Create MultiDict abstraction to separate concerns

--- ch04/simple_todo.ex
+++ ch04/simple_todo.ex
@@ -1,16 +1,11 @@
 defmodule TodoList do
-  def new, do: Map.new
+  def new, do: MultiDict.new
 
   def add_entry(todo_list, date, title) do
-    Map.update(
-      todo_list,
-      date,
-      [title],
-      fn(titles) -> [title | titles] end
-    )
+    MultiDict.add(todo_list, date, title) 
   end
 
   def entries(todo_list, date) do
-    Map.get(todo_list, date, [])
+    MultiDict.get(todo_list, date)
   end
 end
---
+++ ch04/todo-multi_dict.ex
@@ -0,0 +1,16 @@
+defmodule MultiDict do
+  def new, do: Map.new
+
+  def add(dict, key, value) do
+    Map.update(
+      dict,
+      key,
+      [value],
+      &[value | &1]
+    )
+  end
+
+  def get(dict, key) do
+    Map.get(dict, key, [])
+  end
+end

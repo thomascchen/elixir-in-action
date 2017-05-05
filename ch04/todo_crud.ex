b0ee9168f51b15d19e1ca97b194c43288e6a381c
Tom Chen <chen.thomas.c@gmail.com>
2017-05-05 14:38:28 -0400

    Add update_entry and pattern match to enforce TodoEntry struct

--- ch04/todo_crud.ex
+++ ch04/todo_crud.ex
@@ -1,4 +1,8 @@
 # Section 4.2.1
+defmodule TodoEntry do
+  defstruct id: nil, date: nil, title: nil
+end
+
 defmodule TodoList do
   defstruct auto_id: 1, entries: Map.new
 
@@ -9,7 +13,7 @@ defmodule TodoList do
     %TodoList{entries: entries, auto_id: auto_id} = todo_list,
     entry
   ) do
-    entry = Map.put(entry, :id, auto_id)
+    entry = %TodoEntry{} = Map.put(entry, :id, auto_id)
     new_entries = Map.put(entries, auto_id, entry)
 
     %TodoList{todo_list |
@@ -26,5 +30,26 @@ defmodule TodoList do
     |> Enum.map(fn({_, entry}) ->
         entry
       end)
+  end
+
+  # Alternative interface for update_entry
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn(_) -> new_entry end)
+  end
+
+  def update_entry(
+    %TodoList{entries: entries} = todo_list,
+    entry_id,
+    updater_fun
+  ) do
+    case entries[entry_id] do
+      nil -> todo_list
+
+      old_entry ->
+        old_entry_id = old_entry.id
+        new_entry = %TodoEntry{id: ^old_entry_id} = updater_fun.(old_entry) # Nested match requires the result of this function to be a Map and that the id matches the old entry id
+        new_entries = Map.put(entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries }
+    end
   end
 end

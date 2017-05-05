de2dc5298345f312f5e10c691f5b3528d16dd411
Tom Chen <chen.thomas.c@gmail.com>
2017-05-05 10:46:10 -0400

    Add comments on tail recursion

--- ch03/sum_list.ex
+++ ch03/sum_list.ex
@@ -1,6 +1,10 @@
+# Section 3.4.1
+# Recursive approach could potentially lead to running out of memory with
+# a very long list because each time sum is called, new memory is allocated
+# or pushed onto the stack without releasing or popping until the recursion
+# has completed.
 defmodule ListHelper do
   def sum([]), do: 0
-
   def sum([head | tail]) do
     head + sum(tail)
   end
--- ch03/sum_list_tc.ex
+++ ch03/sum_list_tc.ex
@@ -1,4 +1,12 @@
-defmodule ListHelper do
+# Section 3.4.2
+# Tail Recursive approach, based on tail calls (when the last thing a function
+# does is call another function)
+# Elixir treats tail calls specially by performing tail-call optimization
+# Instead of allocating memory, a goto/jump statement happens, and no additional memory
+# is consumed.
+# This approach is preferable when there is a potentially unlimited list
+# But the declarative classical approach may be better in certain situations where that is not the case.
+defmodule ListHelperTR do
   def sum(list) do
     do_sum(0, list)
   end
@@ -8,7 +16,17 @@ defmodule ListHelper do
   end
 
   defp do_sum(current_sum, [head | tail]) do
-    new_sum = head + current_sum
-    do_sum(new_sum, tail)
+    do_sum(head + current_sum, tail)
   end
+
+  # Alternative versions of the same function:
+  # defp do_sum(current_sum, [head | tail]) do
+  #   new_sum = head + current_sum
+  #   do_sum(new_sum, tail)
+  # end
+  #
+  # defp do_sum(current_sum, [head | tail]) do
+  #   head + current_sum
+  #   |> do_sum(tail)
+  # end
 end
